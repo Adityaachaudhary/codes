80390f1f5c586018482bf2db83ee1a9654998517
Adityaachaudhary <91679895+Adityaachaudhary@users.noreply.github.com>
2021-12-06 20:19:29 +0530

    Add files via upload

---
+++ pattern 2 (2).c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+   // printf("GIVE THE NBR");
+    //scanf("%d",&n);
+   { for(i=1;i<=4;i++){
+        for(j=1;j<=5;j++)
+        printf("*");
+        
+        
+       printf("\n"); }}
+       
+    
+    
+
+
+    return 0;
+ 
+}
